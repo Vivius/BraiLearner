1575bab4f89c177b241818f3ecbc6af29df8740d
Carnnack <cedric_th@hotmail.fr>
2019-02-07 11:29:09 +0100

    Ajout de la fonction de jeu

---
+++ src/main/java/game/game.java
@@ -0,0 +1,12 @@
+package game;
+
+import nfc.NfcReader;
+
+public class game {
+    String valeur = "Groupe 5";
+    NfcReader nfcReader = new NfcReader();
+    /*do {
+        System.out.println("test");
+    } while(1 = 1);
+    */
+}
