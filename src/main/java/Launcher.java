a12f95f7b8cd163016ec5221aebec7b5444b5021
Vincent PEILLEX <peillex.vincent@live.fr>
2019-02-08 13:31:07 +0100

    New audio player

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,38 +1,29 @@
-import javafx.application.Application;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.stage.Stage;
+import javazoom.jl.player.Player;
 import nfc.NfcReader;
 
-import java.io.File;
-import java.net.URI;
+import java.io.FileInputStream;
 
-public class Launcher extends Application {
+public class Launcher {
 
     public Launcher() {
 
     }
 
-    public void start(Stage primaryStage) throws Exception {
+    public void start() {
         System.out.println("BraiLearner ready !");
 
         // NfcReader nfcReader = new NfcReader();
         // System.out.println(nfcReader.readCard());
 
-        final File audioFile = new File(getClass().getResource("commons/intro.mp3").getFile());
-        URI uri = audioFile.toURI();
-
-        System.out.println(audioFile.getAbsolutePath());
-
-        final Media media = new Media(uri.toString());
-        final MediaPlayer mediaPlayer = new MediaPlayer(media);
-
-        mediaPlayer.play();
-        mediaPlayer.setOnEndOfMedia(() -> System.exit(0));
-
+        try {
+            Player player = new Player(new FileInputStream(ClassLoader.getSystemResource("commons/intro.mp3").getFile()));
+            player.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-        Application.launch(Launcher.class);
+        new Launcher().start();
     }
 }
