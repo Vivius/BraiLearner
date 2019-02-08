bc0ff820fa3226eac32188ced208521650665d2f
Vincent PEILLEX <peillex.vincent@live.fr>
2019-02-08 16:29:03 +0100

    other test

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -2,10 +2,6 @@ import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 import nfc.NfcReader;
 
-import javax.sound.sampled.*;
-import javax.swing.*;
-import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 public class Launcher {
@@ -19,43 +15,10 @@ public class Launcher {
 
         // NfcReader nfcReader = new NfcReader();
         // System.out.println(nfcReader.readCard());
-        /*
-        try {
-           InputStream audioFileStream = Thread.currentThread()
+
+         final InputStream audio = Thread.currentThread()
                     .getContextClassLoader()
                     .getResourceAsStream("commons/music.wav");
-
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(audioFileStream);
-
-            System.out.println(audioFileStream);
-
-            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(bufferedInputStream);
-
-            for (Mixer.Info info : AudioSystem.getMixerInfo()) {
-                System.out.println(info);
-            }
-
-            Clip clip = AudioSystem.getClip();
-
-
-            clip.open(audioInputStream);
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-
-
-
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (UnsupportedAudioFileException e) {
-            e.printStackTrace();
-        }
-
-        */
-
-         InputStream audio = Thread.currentThread()
-                    .getContextClassLoader()
-                    .getResourceAsStream("commons/intro.mp3");
 
         Player player = null;
         try {
@@ -68,13 +31,6 @@ public class Launcher {
     }
 
     public static void main(String[] args) {
-
-        Thread thread = new Thread(() -> new Launcher().start());
-        thread.start();
-
-        JFrame frame = new JFrame("BraiLearner");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setVisible(true);
+        new Launcher().start();
     }
 }
